b0907f0133537d51f0e774e7cdd2191f2d4092f4
beatorizu <betrizaf@gmail.com>
2019-06-13 22:00:42 -0300

    chore: Add project settings

---
+++ web_scraping/settings.py
@@ -0,0 +1,36 @@
+import re
+
+URL = 'http://compras.dados.gov.br/materiais/v1/materiais.html'
+
+DATABASE_CONN = 'mongodb+srv://{user}:{password}@cluster0-1gfrp.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+attributes_properties = {
+    'Código do Item': {
+        'field': 'item_id',
+        'parser': lambda x: int(x)
+    },
+    'Descrição do Item': {
+        'field': 'item_description',
+        'parser': lambda x: re.sub(r'\s{2,}', '', x)
+    },
+    'Grupo': {
+        'field': 'item_group',
+        'parser': lambda x: x.contents[0].strip()
+    },
+    'Classe': {
+        'field': 'item_class',
+        'parser': lambda x: x.contents[0].strip()
+    },
+    'PDM': {
+        'field': 'pdm',
+        'parser': lambda x: x.contents[0].strip()
+    },
+    'Status': {
+        'field': 'status',
+        'parser': lambda x: x
+    },
+    'Sustentável': {
+        'field': 'is_sustainable',
+        'parser': lambda x: {'Não': False, 'Sim': True}.get(x)
+    }
+}
